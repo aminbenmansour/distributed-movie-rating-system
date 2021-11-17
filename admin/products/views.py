ed6933c8c94c846971e4fa670bc9464ce00d62ff
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2021-11-17 01:46:13 +0100

    define ViewSets

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+
+    # /api/products
+    def list(self, request):
+        pass
+    
+    # /api/products
+    def create(self, request):
+        pass
+
+    # /api/products/<str:id>
+    def retrieve(self, request, pk=None):
+        pass
+
+    # /api/products/<str:id>
+    def update(self, request, pk=None):
+        pass
+
+    # /api/products/<str:id>
+    def delete(self, request, pk=None):
+        pass
