7a5a0ba30bb987dc173137b6a9975f70480fd679
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2021-11-17 02:35:22 +0100

    implementing methods

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,24 +1,41 @@
 # from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
+from .models import Product
+from .serializers import ProductSerializer
 class ProductViewSet(viewsets.ViewSet):
 
     # /api/products
     def list(self, request):
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
     
     # /api/products
     def create(self, request):
-        pass
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    # /api/products/<str:id>
+    # /api/products/<str:pk>
     def retrieve(self, request, pk=None):
-        pass
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
 
-    # /api/products/<str:id>
+    # /api/products/<str:pk>
     def update(self, request, pk=None):
-        pass
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
-    # /api/products/<str:id>
+
+    # /api/products/<str:pk>
     def destroy(self, request, pk=None):
-        pass
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_202_NO_CONTENT)
