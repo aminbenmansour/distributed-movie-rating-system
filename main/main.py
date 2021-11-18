eb65f7894d8331826e11b38dd67e5e9757ec4293
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2021-11-18 01:10:50 +0100

    connect flask to mysql

--- main/main.py
+++ main/main.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
+CORS(app)
 
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
