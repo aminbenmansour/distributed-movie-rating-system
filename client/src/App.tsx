441dc8a74dabcd39e62a32610d8c33110f09a9ab
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2021-11-19 04:09:14 +0100

    even simpler

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Nav from './components/Nav';
 import Menu from './components/Menu';
+import Products from './admin/Products';
 
 import './App.css';
 
@@ -15,32 +16,7 @@ function App() {
           <Menu />
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-
-            <h2>Section title</h2>
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-
-                </tbody>
-              </table>
-            </div>
+            <Products />
           </main>
         </div>
       </div>
