f95e2a308e0bf141a163a5691a2f2a7ff9f772e3
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2021-11-19 16:00:48 +0100

    add main component and refactor the app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,22 @@
 import React from 'react';
 
-import Nav from './components/Nav';
-import Menu from './components/Menu';
 import Products from './admin/Products';
-
+import Main from './main/Main';
 import './App.css';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-
-      <div className="container-fluid">
-        <div className="row">
-          <Menu />
-
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      
             <BrowserRouter>
               <Routes>
+                <Route path='/' element={<Main />} />
                 <Route path='/admin/products' element={<Products />} />
               </Routes>
             </BrowserRouter>
-          </main>
-        </div>
-      </div>
+          
     </div>
   );
 }
